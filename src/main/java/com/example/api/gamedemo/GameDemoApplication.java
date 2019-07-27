6f5b11a4daa1fbb73a2fd44bb9532afe03fc2f8c
anikhar88 <anikhar88@gmail.com>
2019-07-27 20:07:19 +0530

    Added logic in controlelr

--- src/main/java/com/example/api/gamedemo/GameDemoApplication.java
+++ src/main/java/com/example/api/gamedemo/GameDemoApplication.java
@@ -47,7 +47,7 @@ public class GameDemoApplication {
 		responseBody.setMsg(Constants.INITIALIZE);
 		responseBody.setBody(new InnerBody(null, "Player 1", null));
 		System.out.println(responseBody);
-		return  responseBody;
+		return responseBody;
 	}
 
 	private void resetMap() {
@@ -61,36 +61,44 @@ public class GameDemoApplication {
 		com.example.api.models.ResponseBody responseBody = new com.example.api.models.ResponseBody();
 		System.out.println("NODE_CLICKED : " + point);
 
-		// If node-clicked is not started then let's start it
-		if(!state.containsKey(Constants.STATE_START)) {
-			state.put(Constants.STATE_START, point);
-			responseBody.setMsg(Constants.VALID_START_NODE);
-			responseBody.setBody(new InnerBody(null, "Player 2", "Select a second node to complete the line."));
-			System.out.println(responseBody);
-			return  responseBody;
+		//if start node is already there in the state map then add point as end node
+		if(state.containsKey(Constants.STATE_START) && state.get(Constants.STATE_START) != null) {
+			state.put(Constants.STATE_END, point);
 		}
 
-		// if node-clicked event is already started but not the end point
-		if(state.containsKey(Constants.STATE_START) && !state.containsKey(Constants.STATE_END)) {
-			state.put(Constants.STATE_END, point);
+		// If node-clicked is not started then let's start it
+		if (!state.containsKey(Constants.STATE_START)) {
+			state.put(Constants.STATE_START, point);
+			responseBody.setMsg(Constants.VALID_START_NODE);
+			responseBody.setBody(new InnerBody(null, (String) state.get("playerKey"), "Select a second node to complete the line."));
+			System.out.println(responseBody);
+			return responseBody;
+		}
+		//if user selected the start point but it's not an adjuscent then throw error
+		// if user selected node is not adjuscent then should be INVALID_NODE
+		if (!adjuscentPoints.containsKey(point) || (state.get(Constants.STATE_START) != null) && state.get(Constants.STATE_END) != null) {
+			state.remove(Constants.STATE_START);
+			responseBody.setMsg(Constants.INVALID_END_NODE);
+			responseBody.setBody(new InnerBody(null, (String) state.get(Constants.PLAYER_1), "Invalid move! Try again."));
+			System.out.println(responseBody);
+			return responseBody;
+		}
+
+
+		// if user is selected proper adjuscent node
+		if (adjuscentPoints.containsKey(point)) {
 			Point start = (Point) state.get(Constants.STATE_START);
-			Point end = (Point) state.get(Constants.STATE_END);
+			//end = (Point) state.get(Constants.STATE_END);
 			Line line = new Line(start, point);
 			responseBody.setMsg(Constants.VALID_START_NODE);
 			responseBody.setBody(new InnerBody(line, "Player 2", "Select a second node to complete the line."));
 			System.out.println(responseBody);
-			return  responseBody;
+			return responseBody;
 		}
 
-		// if user selected node is not adjuscent then should be INVALID_NODE
-		if(!adjuscentPoints.containsKey(point)) {
-
-			responseBody.setMsg(Constants.INVALID_END_NODE);
-			responseBody.setBody(new InnerBody(null, "Player 2", "Invalid move!."));
-			System.out.println(responseBody);
-			return  responseBody;
-		} else {
-
+		// if node-clicked event is already started but not the end point
+		if (state.containsKey(Constants.STATE_START) && !state.containsKey(Constants.STATE_END)) {
+			state.put(Constants.STATE_END, point);
 			Point start = (Point) state.get(Constants.STATE_START);
 			Point end = (Point) state.get(Constants.STATE_END);
 			Line line = new Line(start, point);
@@ -99,6 +107,21 @@ public class GameDemoApplication {
 			System.out.println(responseBody);
 			return responseBody;
 		}
+
+		Point start = (Point) state.get(Constants.STATE_START);
+		Point end = (Point) state.get(Constants.STATE_END);
+
+		// if user already selected 1 line and tries to select another point then it should be either START or END should be clicked first
+		if (!start.equals(point) || !end.equals(point)) {
+			// invalid move!!
+			responseBody.setMsg(Constants.INVALID_START_NODE);
+			responseBody.setBody(new InnerBody(null, "Player 2", "Not a valid starting position."));
+			System.out.println(responseBody);
+			return responseBody;
+		}
+		// user selected already from the start or end node
+		// then we need to update start and end in the current state.
+		return null;
 
 	}
 
